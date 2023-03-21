04167df92e761929b7fd4f1c3fcd39892b87de6c
Muhammad Abiyyu Farhan <muh.abiyyufarhan@gmail.com>
2023-03-21 15:19:39 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+const notes = [];
+ 
+module.exports = notes;
